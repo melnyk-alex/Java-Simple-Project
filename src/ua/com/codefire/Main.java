daa3a5de67625033c5d931517ec8af889ba85ff5
Alex Melnyk <melnyk@codefire.com.ua>
2016-06-07 16:39:26 +0300

    Add project files.

---
+++ src/ua/com/codefire/Main.java
@@ -0,0 +1,20 @@
+/*
+ * CodeFireUA public license.
+ */
+
+package ua.com.codefire;
+
+/**
+ *
+ * @author CodeFireUA <edu@codefire.com.ua>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO: Burn your code here...
+    }
+
+}
